0adb1d1b5bc6a9cf6909e819344c4b385cfda14d
Nishit <nishitranjan41@gmail.com>
2022-08-25 14:59:52 +0530

    this is stght

--- src/com/bridgelab/basics/oops/StockAccount.java
+++ src/com/bridgelab/basics/oops/StockAccount.java
@@ -1,10 +1,17 @@
 package com.bridgelab.basics.oops;
 
+import java.util.Scanner;
+
 public class StockAccount {
     public static void main(String[] args) {
         StockPortfolio sp = new StockPortfolio();
         sp.stockDetails();
         System.out.println(sp.listOfStocks);
         sp.valueOfStocks();
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.println("enter total money in account ");
+        int totalMoneyInAccount = sc.nextInt();
+        sp.debit( totalMoneyInAccount );
     }
 }
--- src/com/bridgelab/basics/oops/StockPortfolio.java
+++ src/com/bridgelab/basics/oops/StockPortfolio.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 
 public class StockPortfolio {
     ArrayList<Stock> listOfStocks = new ArrayList<>();
-   void stockDetails(){
+
+    void stockDetails() {
         System.out.println("Enter no of stocks");
         Scanner sc = new Scanner(System.in);
         int numberOfStocks = sc.nextInt();
-        for (int i = 0 ; i < numberOfStocks ;  i++){
+        for (int i = 0; i < numberOfStocks; i++) {
             Stock stock = new Stock();
             System.out.println("Enter stock name");
             stock.setStockName(sc.next());
@@ -19,19 +20,29 @@ public class StockPortfolio {
             stock.setSharePrice(sc.nextInt());
             listOfStocks.add(stock);
         }
-   }
-   void valueOfStocks()
-   {
-       double totalValue = 0;
-       for (Stock stock : listOfStocks) {
+    }
+
+    void valueOfStocks() {
+        double totalValue = 0;
+        for (Stock stock : listOfStocks) {
             double stockValue = stock.getNumOfShares() * stock.getSharePrice();
             System.out.println("stock value of " + stock.getStockName() + " " + stockValue);
             totalValue += stockValue;
-       }
+        }
         System.out.println("value of  total stock is: " + totalValue);
-   }
-   void displayData(){
-       for (Stock stock : listOfStocks )
-           System.out.println(stock);
-   }
+    }
+    void debit( double totalMoneyInAccount ){
+        System.out.println("enter debit money in account");
+        Scanner sc = new Scanner(System.in);
+        int debittedMoney = sc.nextInt();
+        if( debittedMoney < totalMoneyInAccount )
+        {
+            System.out.println("you can collect money =>" + debittedMoney);
+        }
+        else
+        {
+            System.out.println("you have exceeded the account balance to debit");
+            System.out.println("your available is =>" + totalMoneyInAccount);
+        }
+    }
 }
